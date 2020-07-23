47c3fd7785b18418c79c56ce15953eb3bb7dd573
jynadal <nadaljeanyves@hotmail.fr>
2020-07-23 10:36:18 +0200

    build file added

---
+++ build/precache-manifest.63f7b98f4e5a91be9310bd0e9fdc0319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74729516dce2356603ed5032788e3e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fade6fcf5ad52118712c",
+    "url": "/static/css/main.67853a8a.chunk.css"
+  },
+  {
+    "revision": "ada21334d60d3f98da75",
+    "url": "/static/js/2.2c3d9d6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c3d9d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fade6fcf5ad52118712c",
+    "url": "/static/js/main.064fcda1.chunk.js"
+  },
+  {
+    "revision": "79eefbb7815170a52a15",
+    "url": "/static/js/runtime-main.ea70b20a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
